a5a0ad1553eb49959c1edd75581b81769bd35558
Laurent Nguyen <laurent.nguyen@microsoft.com>
2022-09-13 11:33:55 +0200

    Download ADS instead of VSCode in order to run unit tests

--- src/test/runTest.ts
+++ src/test/runTest.ts
@@ -1,6 +1,9 @@
 import * as path from "path";
-
-import { runTests } from "vscode-test";
+import * as fs from "fs";
+import { runTests } from "@vscode/test-electron";
+import fetch from "node-fetch";
+import * as tar from "tar";
+import { Unzipper } from "../BinaryInstallUtil/zip";
 
 async function main() {
   try {
@@ -13,11 +16,85 @@ async function main() {
     const extensionTestsPath = path.resolve(__dirname, "./suite/index");
 
     // Download VS Code, unzip it and run the integration test
-    await runTests({ extensionDevelopmentPath, extensionTestsPath });
+    // const testpath = await downloadAndUnzipVSCode("1.35.0");
+    // console.log("testpath", testpath);
+
+    const unitTestDirectory = path.resolve(__dirname, "./ads-binary");
+    const downloadUrl = getDownloadUrl();
+    console.log(`Downloading: ${downloadUrl} to ${unitTestDirectory}`);
+
+    const filename = await downloadADSFile(getDownloadUrl(), unitTestDirectory);
+    console.log(`Successfully downloaded ${filename}`);
+
+    console.log(`Extracting ${filename}...`);
+    await extract(filename, unitTestDirectory);
+
+    const adsExecutablePath = path.resolve(unitTestDirectory, getADSExecutableFilename());
+    if (!fs.existsSync(adsExecutablePath)) {
+      console.error(`Could not find ${adsExecutablePath}`);
+      process.exit(1);
+    }
+
+    await runTests({ vscodeExecutablePath: adsExecutablePath, extensionDevelopmentPath, extensionTestsPath });
+
+    console.log("Finished");
   } catch (err) {
-    console.error("Failed to run tests");
+    console.error("Failed to run tests", err);
     process.exit(1);
   }
 }
+
+const getADSExecutableFilename = () => {
+  switch (process.platform) {
+    case "win32":
+      return "azuredatastudio.exe";
+    default:
+      return "azuredatastudio";
+  }
+};
+
+const getDownloadUrl = () => {
+  switch (process.platform) {
+    case "darwin":
+      return "https://go.microsoft.com/fwlink/?linkid=2204569";
+    case "win32":
+      return "https://go.microsoft.com/fwlink/?linkid=2204772";
+    default:
+      /* linux */
+      return "https://go.microsoft.com/fwlink/?linkid=2204773";
+  }
+};
+
+const downloadADSFile = async (url: string, targetFolder: string): Promise<string> => {
+  if (fs.existsSync(targetFolder)) {
+    fs.rmdirSync(targetFolder);
+  }
+  fs.mkdirSync(targetFolder);
+
+  const response = await fetch(url);
+  // Get filename from URL
+  const fullPath = path.resolve(targetFolder, response.url.substring(response.url.lastIndexOf("/") + 1));
+  console.log(`Saving to ${fullPath}...`);
+  const fileStream = fs.createWriteStream(fullPath);
+  await new Promise((resolve, reject) => {
+    response.body.pipe(fileStream);
+    response.body.on("error", reject);
+    fileStream.on("finish", resolve);
+  });
+
+  return fullPath;
+};
+
+const extract = (archivePath: string, targetPath: string): Promise<void> => {
+  if (archivePath.match(/\.tar\.gz|\.tar|\.gz$/i)) {
+    return tar.x({
+      file: archivePath,
+      cwd: targetPath,
+    });
+  } else {
+    // Default to zip extracting if it's not a tarball
+    return new Unzipper().extract(archivePath, targetPath);
+  }
+};
 
 main();
