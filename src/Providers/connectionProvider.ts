8ba54332b55ece7c769a908215805ab90487df6c
Bharath Palaksha <bharathp87@gmail.com>
2023-03-27 21:24:10 +0530

    getConnectionString in connection provider for multiple connections (#67)
    * connection string map
    
    * Removing appcontext
    
    * adding this.backendservice to method call
    
    * run prettier
    
    ---------
    
    Co-authored-by: Bharath Palaksha <bhpalaks@microsoft.com>

--- src/Providers/connectionProvider.ts
+++ src/Providers/connectionProvider.ts
@@ -19,8 +19,8 @@ export class ConnectionProvider implements azdata.ConnectionProvider {
   // Maintain current connections server <--> connectionUri
   private connectionUriToServerMap = new Map<string, string>();
 
-  // Maintain connection string to return through connection provider
-  private connectionString: string | undefined;
+  // Maintain current connections - connection string <--> connectionUri
+  private connectionUriToConnectionStringMap = new Map<string, string | undefined>();
 
   private onConnectionCompleteEmitter: vscode.EventEmitter<azdata.ConnectionInfoSummary> = new vscode.EventEmitter();
   onConnectionComplete: vscode.Event<azdata.ConnectionInfoSummary> = this.onConnectionCompleteEmitter.event;
@@ -45,23 +45,25 @@ export class ConnectionProvider implements azdata.ConnectionProvider {
     const connectionOptions = convertToConnectionOptions(connectionInfo);
     this.connectionUriToServerMap.set(connectionUri, server);
 
+    let connectionString;
     try {
-      this.connectionString = await this.backendService.retrieveConnectionStringFromConnectionOptions(
+      connectionString = await this.backendService.retrieveConnectionStringFromConnectionOptions(
         connectionOptions,
         false
       );
+      this.connectionUriToConnectionStringMap.set(connectionUri, connectionString);
     } catch (e) {
       showErrorMessage((e as { message: string }).message);
       return false;
     }
 
-    if (!this.connectionString) {
+    if (!connectionString) {
       showErrorMessage(localize("failRetrieveCredentials", "Unable to retrieve credentials"));
       return false;
     }
 
     try {
-      if (!(await this.backendService.connect(server, this.connectionString))) {
+      if (!(await this.backendService.connect(server, connectionString))) {
         vscode.window.showErrorMessage(localize("failConnect", "Failed to connect"));
         return false;
       }
@@ -102,6 +104,7 @@ export class ConnectionProvider implements azdata.ConnectionProvider {
 
     this.backendService.disconnect(this.connectionUriToServerMap.get(connectionUri)!);
     this.connectionUriToServerMap.delete(connectionUri);
+    this.connectionUriToConnectionStringMap.delete(connectionUri);
 
     return Promise.resolve(true);
   }
@@ -125,8 +128,9 @@ export class ConnectionProvider implements azdata.ConnectionProvider {
   }
   getConnectionString(connectionUri: string, includePassword: boolean): Promise<string> {
     console.log("ConnectionProvider.getConnectionString");
-    if (this.connectionString) {
-      return Promise.resolve(this.connectionString);
+    const connectionString = this.connectionUriToConnectionStringMap.get(connectionUri);
+    if (connectionString) {
+      return Promise.resolve(connectionString);
     } else {
       return Promise.resolve("");
     }
