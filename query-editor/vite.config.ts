2911621955dad188df5f3ccab0f2228fa142f046
Laurent Nguyen <laurent.nguyen@microsoft.com>
2023-07-28 13:19:15 +0200

    Update vite config to output one chunk

--- query-editor/vite.config.ts
+++ query-editor/vite.config.ts
@@ -4,4 +4,17 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    sourcemap: true,
+    rollupOptions: {
+      output: {
+        manualChunks: () => 'one-chunk',
+        assetFileNames: 'assets/query-editor.[ext]',
+        entryFileNames: 'assets/query-editor.js'
+      }
+    }
+  },
+  css: {
+    devSourcemap: true,
+  },
 })
